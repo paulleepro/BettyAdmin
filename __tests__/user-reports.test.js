d58915a9478c8732a6fd1ca02e3b2b204d5997e7
Paul Lee <plee6950@gmail.com>
2021-06-17 23:23:27 +0500

    user-reports unit tests

--- __tests__/user-reports.test.js
+++ __tests__/user-reports.test.js
@@ -1,14 +1,14 @@
 import React from "react";
-import { cleanup, render, fireEvent } from "@testing-library/react";
+import { cleanup, render } from "@testing-library/react";
 import { MockedProvider } from "@apollo/client/testing";
 import user from "@testing-library/user-event";
 import { Provider } from "react-redux";
 import store from "../store";
-import Home from "../pages/ban-users";
+import Home from "../pages/user-reports";
 
 afterEach(cleanup);
 
-describe("Home component test suit", () => {
+describe("UserReports component test suit", () => {
   it("Should take a snapshot", () => {
     const { asFragment } = render(
       <MockedProvider>
@@ -40,32 +40,7 @@ describe("Home component test suit", () => {
     );
     const title = getByTestId("title");
     expect(title).toBeInTheDocument();
-    expect(title).toHaveTextContent("Ban Users");
-  });
-
-  it("Correct button rendered", () => {
-    const { getByTestId } = render(
-      <MockedProvider>
-        <Provider store={store}>
-          <Home />
-        </Provider>
-      </MockedProvider>
-    );
-    const button = getByTestId("button");
-    expect(button).toBeInTheDocument();
-    expect(button).toHaveTextContent("Ban User");
-  });
-
-  it("Fire button click event", () => {
-    const { getByTestId } = render(
-      <MockedProvider>
-        <Provider store={store}>
-          <Home />
-        </Provider>
-      </MockedProvider>
-    );
-    const button = getByTestId("button");
-    fireEvent.click(button);
+    expect(title).toHaveTextContent("User Reports");
   });
 
   it("search field rendered", () => {
